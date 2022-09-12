37603faff7148f5f7dbbf5240a15da7db586e90c
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-09-12 01:40:02 -0700

    testing the JS code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
-const links = [
-    {
-        label:  "Week 1 - Doing Stuff",
-        url: "week1/story_editor.html"
-    }
-]
+// Creating the Ordered List function
+function myList() {
+
+    // Defining the list of links
+    const links = [
+        {
+            label:  "Week 1 - Doing Stuff",
+            url: "week1/story_editor.html"
+        },
+        {
+            label: "Week 1 - Notes",
+            url: "week1/notes.html"
+        }
+    ]
+
+    links.forEach(v => {
+        let li = document.createElement("li");
+
+        let a = document.createElement("a");
+
+        let text = document.createTextNode(v.label);
+
+        a.appendChild(text);
+        a.setAttribute("href", v.url);
+        li.appendChild(a);
+        let list = document.getElementById("mainIndex");
+
+        list.appendChild(li);
+    })
+}
+
+myList();
