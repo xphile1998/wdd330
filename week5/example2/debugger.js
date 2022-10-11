c95ab07fedbbc646fd02b9a6659b44a89f6f2d13
xphile1998 <eron.whitefield@gmail.com>
2022-10-11 11:57:23 -0700

    Finished the reading.

---
+++ week5/example2/debugger.js
@@ -0,0 +1,15 @@
+function amIOldEnough(age){
+    debugger;
+        if (age < 12) {
+        debugger;
+        return 'No, sorry.';
+        } else if (age < 18) {
+        debugger;
+        return 'Only if you are accompanied by an adult.';
+        } else {
+        debugger;
+        return 'Yep, come on in!';
+    }
+}
+
+amIOldEnough(16);
