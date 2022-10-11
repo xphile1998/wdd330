400cce0e9cff8e1ba34779e6af7b6e1e3b49e3ae
xphile1998 <eron.whitefield@gmail.com>
2022-10-11 16:26:25 -0700

    Corrected some spelling & grammar errors

--- week5/hiking/hiking-start/hiking-start.js
+++ week5/hiking/hiking-start/hiking-start.js
@@ -9,7 +9,7 @@ const hikeList = [
     description:
       "Beautiful short hike along the Bechler river to Bechler Falls",
     directions:
-      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
+      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
   },
   {
     name: "Teton Canyon",
@@ -19,7 +19,7 @@ const hikeList = [
     difficulty: "Easy",
     description: "Beautiful short (or long) hike through Teton Canyon.",
     directions:
-      "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+      "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Road for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
   },
   {
     name: "Denanda Falls",
