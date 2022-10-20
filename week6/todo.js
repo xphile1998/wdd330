40d3044dacf4d6ed46901fb44a5593d6bc0e3e75
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-10-20 12:22:14 -0700

    Working through possible changes

---
+++ week6/todo.js
@@ -0,0 +1,28 @@
+const todoObjectList = [];
+
+class Todo_Class {
+    constructor(item){
+        this.ulElement =item;
+    }
+
+    add() {
+
+    }
+
+    done_undone(x) {
+
+    }
+
+    deleteElement(z) {
+
+    }
+
+    display() {
+
+    }
+}
+
+const listSection = document.querySelector("#myTasks");
+
+myTodoList = new Todo_Class(listSection);
+
