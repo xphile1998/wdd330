fa4be4c02f541ed12749905af6efbf8ac3a504cd
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-10-22 12:21:22 -0700

    Finished the HTML for version 4

--- week6/todo_list/new-todo.js
+++ week6/todo_list/new-todo.js
@@ -7,3 +7,22 @@
 // Import the necessary helper files
 // import lStorage from "./ls.js";
 
+let todoTask = document.querySelector("#input");
+let todoList = document.querySelector("#tasks");
+let addTodo = document.querySelector("#addBtn");
+
+addTodo.addEventListener("click", addTask);
+
+function addTask(event) {
+    event.preventDefault();
+
+    // let todoDiv = document.createElement("div");
+    // todoDiv.classList.add("task");
+
+    let todoLi = document.createElement("li");
+    todoLi.classList.add("task");
+    todoLi.innerHTML = '<input type="checkbox" class="checkbox">';
+    todoLi.innerText = todoTask.value;
+    todoLi.innerHTML = '<p class="close">X</p>';
+    todoList.appendChild(todoLi);
+}
