654f25b97f172389d567ecd5c69c30662a5c1d4d
xphile1998 <eron.whitefield@gmail.com>
2022-10-26 17:55:34 -0700

    Ready for team meeting.

--- week7/team_activity/comments.js
+++ week7/team_activity/comments.js
@@ -1,25 +1,45 @@
 // commentModel code
-
+class commentModel {
+    // Build the constructor
 
     // getComments code
 
-
     // addComment code
 
+}
 
 // writeComments to LS code
-
+function writeComments(key, data) {
+    window.localStorage.setItem(key, JSON.stringify(data));
+}
 
 // readComments from LS code
+function readComments(key) {
+    return JSON.parse(window.localStorage.getItem(key));
+}
+
+// Creating a comment constant for use in the Comments class
+const commentUL = `
+<div class="addComment">
+    <h2>Add a Comment</h2>
+    <input type="text" id="commentEntry" />
+    <button id="commentSubmit">Comment</button>
+</div>
+<h2>Comments</h2>
+<ul class="comments"></ul>
+`;
+
+// Render the comment list
 
 
 // Create the Comments class
-
+class Comments {
+    // define the constructor
 
     // addSubmitListener
-
-
+    
     // showCommentList
-
+    
+}
 
 export default Comments;
